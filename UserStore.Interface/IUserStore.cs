8f5158171537e49ff87f5d504122fbe1c34ff41c
Anders Lybecker <anders@lybecker.com>
2018-04-25 14:45:20 +0200

    Implemented the UserStore persistence logic and upated the interface/contract with CRUD operations.

--- UserStore.Interface/IUserStore.cs
+++ UserStore.Interface/IUserStore.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.ServiceFabric.Services.Remoting;
 using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
 using System.Threading.Tasks;
+using Common;
 
 [assembly: FabricTransportServiceRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
 namespace UserStore.Interface
 {
     public interface IUserStore : IService
     {
-        Task<string> HelloWorldAsync();
+        Task<User> GetUserAsync(string userId);
+        Task<string> AddUserAsync(User user);
+        Task<bool> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(string userId);
     }
 }
--- UserStore/UserStore.cs
+++ UserStore/UserStore.cs
@@ -5,23 +5,28 @@ using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 using UserStore.Interface;
+using Common;
+using Microsoft.ServiceFabric.Data.Collections;
+using System;
 
 namespace UserStore
 {
     /// <summary>
-    /// A statefull service 
+    /// A statefull service, used to store Users. Access via binary remoting based on the V2 Remoting stack
     /// </summary>
     internal sealed class UserStore : StatefulService, IUserStore
     {
+        private string _storeName = "UserStore";
+
         public UserStore(StatefulServiceContext context)
             : base(context)
         { }
 
         /// <summary>
-        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// Standard implementation for service endpoints using the V2 Remoting stack. See more: https://aka.ms/servicefabricservicecommunication
         /// </summary>
         /// <remarks>
-        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// Iterates over the ServiceManifest.xml and expects a endpoint with name ServiceEndpointV2 to expose the service over Remoting.
         /// </remarks>
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
@@ -29,9 +34,76 @@ namespace UserStore
             return this.CreateServiceRemotingReplicaListeners();
         }
 
-        public Task<string> HelloWorldAsync()
+        public async Task<User> GetUserAsync(string userId)
         {
-            return Task.FromResult("Hello!");
+            IReliableDictionary<string, User> users =
+               await this.StateManager.GetOrAddAsync<IReliableDictionary<string, User>>(_storeName);
+
+            User user = null;
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var tryUser = await users.TryGetValueAsync(tx, userId);
+                if (tryUser.HasValue)
+                {
+                    user = tryUser.Value;
+                }
+                await tx.CommitAsync();
+            }
+            return user;
+        }
+
+        public async Task<string> AddUserAsync(User user)
+        {
+            IReliableDictionary<string, User> users =
+              await this.StateManager.GetOrAddAsync<IReliableDictionary<string, User>>(_storeName);
+
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                await users.AddAsync(tx, user.Id, user);
+                await tx.CommitAsync();
+            }
+            return user.Id;
+        }
+
+        public async Task<bool> UpdateUserAsync(User user)
+        {
+            IReliableDictionary<string, User> users =
+              await this.StateManager.GetOrAddAsync<IReliableDictionary<string, User>>(_storeName);
+
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var current = await users.TryGetValueAsync(tx, user.Id, LockMode.Update);
+                if (current.HasValue)
+                {
+                    return await users.TryUpdateAsync(tx, user.Id, user, current.Value);
+                }
+                else
+                {
+                    throw new ApplicationException($"Cannot update non existent user '{user.Id}'");
+                }
+            }
+        }
+        
+        public async Task<bool> DeleteUserAsync(string userId)
+        {
+            IReliableDictionary<string, User> users =
+              await this.StateManager.GetOrAddAsync<IReliableDictionary<string, User>>(_storeName);
+
+            bool removed;
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var result = await users.TryRemoveAsync(tx, userId);
+                if (result.HasValue)
+                {
+                    removed = true;
+                }
+                else
+                {
+                    removed = false;
+                }
+                await tx.CommitAsync();
+            }
+            return removed;
         }
     }
 }
