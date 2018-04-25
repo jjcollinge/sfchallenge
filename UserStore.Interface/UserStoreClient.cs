5d4d6909a2e5980b4adafa4d780c4a205bd3b43e
Anders Lybecker <anders@lybecker.com>
2018-04-25 16:04:16 +0200

    Added a partition aware UserStoreClient for easy use of UserStore

---
+++ UserStore.Interface/UserStoreClient.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace UserStore.Interface
+{
+    public class UserStoreClient : IUserStore
+    {
+        private readonly ServiceProxyFactory _serviceProxyFactory;
+        private readonly Uri _userStoreServiceUri;
+        public UserStoreClient()
+        {
+            var settings = new OperationRetrySettings(
+                maxRetryBackoffIntervalOnNonTransientErrors: TimeSpan.FromSeconds(2.0), 
+                maxRetryBackoffIntervalOnTransientErrors: TimeSpan.FromSeconds(2.0), 
+                defaultMaxRetryCount: 5);
+
+            _serviceProxyFactory = new ServiceProxyFactory(settings);
+
+            var serviceUriBuilder = new ServiceUriBuilder("UserStore");
+            _userStoreServiceUri = serviceUriBuilder.ToUri();
+        }
+
+        public async Task<string> AddUserAsync(User user)
+        {
+            var userStoreProxy = GetUserStoreProxy(user);
+
+            return await userStoreProxy.AddUserAsync(user);
+        }
+
+        public async Task<bool> DeleteUserAsync(string userId)
+        {
+            var userStoreProxy = GetUserStoreProxy(userId);
+
+            return await userStoreProxy.DeleteUserAsync(userId);
+        }
+
+        public async Task<User> GetUserAsync(string userId)
+        {
+            var userStoreProxy = GetUserStoreProxy(userId);
+
+            return await userStoreProxy.GetUserAsync(userId);
+        }
+
+        public async Task<bool> UpdateUserAsync(User user)
+        {
+            var userStoreProxy = GetUserStoreProxy(user);
+
+            return await userStoreProxy.UpdateUserAsync(user);
+        }
+        private IUserStore GetUserStoreProxy(string userId)
+        {
+            var servicePartition = GetServicePartition(userId);
+            return _serviceProxyFactory.CreateServiceProxy<IUserStore>(_userStoreServiceUri, servicePartition);
+        }
+
+        private IUserStore GetUserStoreProxy(User user)
+        {
+            var servicePartition = GetServicePartition(user);
+            return _serviceProxyFactory.CreateServiceProxy<IUserStore>(_userStoreServiceUri, servicePartition);
+        }
+
+        private ServicePartitionKey GetServicePartition(User user)
+        {
+            return GetServicePartition(user.Id);
+        }
+
+        private ServicePartitionKey GetServicePartition(string userId)
+        {
+            var hash = Hash(userId);
+            return new ServicePartitionKey(hash);
+        }
+
+        /// <summary>
+        /// Generates a FNV hash (non-cryptographic) based on a string with evenly distributed output. 
+        /// </summary>
+        private long Hash(string input)
+        {
+            input = input.ToUpperInvariant();
+            var value = Encoding.UTF8.GetBytes(input);
+            ulong hash = 14695981039346656037;
+            for (int i = 0; i < value.Length; ++i)
+            {
+                hash ^= value[i];
+                hash *= 1099511628211;
+            }
+            return (long)hash;
+        }
+    }
+}
