be078d558a1e2f271cebb731b36a0ca0ddb5b177
jjcollinge <jonathancollinge@live.com>
2018-06-07 08:50:33 +0100

    Updated xplat build script to work based on script location

--- Scripts/xplat-build.ps1
+++ Scripts/xplat-build.ps1
@@ -6,38 +6,47 @@ function New-TemporaryDirectory {
     New-Item -ItemType Directory -Path (Join-Path $parent $name)
 }
 
-$publishingDir = "./publish"
+$publishDirPath = "${PSScriptRoot}/../publish"
+$appPkgRootPath = "${PSScriptRoot}/../Exchange/ApplicationPackageRoot"
+$appManifestPath = "${appPkgRootPath}/ApplicationManifest.xml"
 
-Write-Host -ForegroundColor Blue "Cleaning existing publishing dir..."
+$ErrorActionPreference = "Stop"
 
-if (Test-Path $publishingDir) {
-    Remove-Item -Recurse $publishingDir -Force
+if (!(Test-Path $appManifestPath))
+{
+    Write-Host -ForegroundColor Red "Cannot find ApplicationManifest.xml at expected path: ${appManifestPath}"
+    exit
 }
 
-New-Item -Path $publishingDir -ItemType Directory
+if (Test-Path $publishDirPath) {
+    Write-Host -ForegroundColor Blue "Cleaning existing publishing directory..."
+    Remove-Item -Recurse $publishDirPath -Force
+}
 
-Copy-Item -Path "./Exchange/ApplicationPackageRoot/*" -Recurse -Destination $publishingDir
-
+New-Item -Path $publishDirPath -ItemType Directory
+Copy-Item -Path "${appPkgRootPath}/*" -Recurse -Destination $publishDirPath
 Write-Host -ForegroundColor Blue "Building projects..."
 
-$projects = Get-ChildItem **/*.csproj
-foreach ($p in $projects){
-    $projectDir = $p.Directory.FullName
-    $packageRootDir = Join-Path $projectDir "PackageRoot"
-    if (-Not (Test-Path $packageRootDir)){
-        Write-Host -ForegroundColor Gray "Skipping $p as not SF service..."
+$projects = Get-ChildItem "${PSScriptRoot}/../**/*.csproj"
+foreach ($project in $projects){
+    $projectDirPath = $project.Directory.FullName
+    $packageRootDirPath = Join-Path $projectDirPath "PackageRoot"
+
+    if (-Not (Test-Path $packageRootDirPath)){
+        Write-Host -ForegroundColor Gray "Skipping $project as it is not a Service Fabric service..."
         continue
     }
 
     ## Create code.zip and copy to publish dir
-    $pkgFolderName = $p.Name -replace ".csproj", "Pkg"
-    $projectPkgFolder = Join-Path $publishingDir $pkgFolderName
+    $pkgFolderName = $project.Name -replace ".csproj", "Pkg"
+    $projectPkgFolder = Join-Path $publishDirPath $pkgFolderName
     New-Item -Path $projectPkgFolder -ItemType Directory
 
     #Create temp dir for build output
     $tempDir = New-TemporaryDirectory
+
     #Build service into staging dir    
-    dotnet publish -c "RELEASE" -r "win7-x64" -o $tempDir.FullName $p.FullName
+    dotnet publish -c "RELEASE" -r "win7-x64" -o $tempDir.FullName $project.FullName
 
     #Compress the output into the publish dir for the service
     Compress-Archive -Path (Join-Path $tempDir.FullName "*")  -DestinationPath (Join-Path $projectPkgFolder "Code.zip") -CompressionLevel Optimal
@@ -45,8 +54,11 @@ foreach ($p in $projects){
     Remove-Item -Recurse $tempDir.FullName
 
     ## Create config.zip in publish dir
-    Compress-Archive -Path (Join-Path (Join-Path $packageRootDir "Config") "*")  -DestinationPath (Join-Path $projectPkgFolder "Config.zip")
+    Compress-Archive -Path (Join-Path (Join-Path $packageRootDirPath "Config") "*")  -DestinationPath (Join-Path $projectPkgFolder "Config.zip")
     
     ## Copy service manifest
-    Copy-Item -Path (Join-Path $packageRootDir "ServiceManifest.xml") -Destination $projectPkgFolder
+    Copy-Item -Path (Join-Path $packageRootDirPath "ServiceManifest.xml") -Destination $projectPkgFolder
 }
+
+Write-Host -ForegroundColor Green "Packing complete, your Service Fabric application package can be found at:"
+Write-Host -ForegroundColor Green "${publishDirPath}"
